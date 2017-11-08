56346d5e1844084a2693d53298f12f16fe8dcf7a
jgiacomini <archeone@gmail.com>
2017-11-08 22:54:46 +0100

    Now Plugin.Logs.Abstraction is a Netstandard library
    Add Plug.Logs.NetStandard Lig

--- src/Plugin.Logs.Abstraction/ILogService.cs
+++ src/Plugin.Logs.Abstraction/ILogService.cs
@@ -4,33 +4,41 @@ using Plugin.Logs.Model;
 
 namespace Plugin.Logs
 {
-	/// <summary>
-	/// Represent a Log service
-	/// </summary>
-	/// <seealso cref="System.IDisposable" />
-	public interface ILogService : IDisposable
-	{
-		/// <summary>
-		/// Gets or sets the nb month to keep.
-		/// </summary>
-		/// <value>
-		/// The nb month to keep.
-		/// </value>
-		uint NbDaysToKeep { get; }
+    /// <summary>
+    /// Represent a Log service
+    /// </summary>
+    /// <seealso cref="System.IDisposable" />
+    public interface ILogService : IDisposable
+    {
+        /// <summary>
+        /// Gets or sets the nb month to keep.
+        /// </summary>
+        /// <value>
+        /// The nb month to keep.
+        /// </value>
+        uint NbDaysToKeep { get; }
 
-		/// <summary>
-		/// Logs the specified message.
-		/// </summary>
-		/// <param name="message">The message.</param>
-		/// <param name="logLevel">The log level.</param>
-		void Log(string message, LogLevel logLevel = LogLevel.Information);
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="logLevel">The log level.</param>
+        void Log(string message, LogLevel logLevel = LogLevel.Information);
 
-		/// <summary>
-		/// Logs the specified exception.
-		/// </summary>
+        /// <summary>
+        /// Logs the specified exception.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <param name="logLevel">The log level.</param>
+        void Log(Exception exception, LogLevel logLevel = LogLevel.Error);
+
+        /// <summary>
+        /// Logs the specified exception and his custom message.
+        /// </summary>
+        /// <param name="message">The message</param>
 		/// <param name="exception">The exception.</param>
 		/// <param name="logLevel">The log level.</param>
-		void Log(Exception exception, LogLevel logLevel = LogLevel.Error);
+        void Log(string message, Exception exception, LogLevel logLevel = LogLevel.Error);
 
         /// <summary>
         /// Purges the old days asynchronous.
@@ -43,5 +51,5 @@ namespace Plugin.Logs
         /// </summary>
         /// <returns>return a task</returns>
         Task FlushAsync();
-	}
+    }
 }
